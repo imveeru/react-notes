0d285ba2a4270f0bbafa2b28829e1b1bbdfcb238
imvee <vind.veeru@gmail.com>
2021-06-14 23:43:39 +0530

    :sparkles: Added Tooltip.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {BsMoon} from 'react-icons/bs'
 import {MdWbSunny} from 'react-icons/md'
+import Tippy from '@tippy.js/react'
+import 'tippy.js/dist/tippy.css'
 
 function Header({handleDarkMode,darkMode}) {
     return (
         <div className="header">
             <h1>Notes</h1>
-            <button
-                className="togg-btn"
-                onClick={()=>handleDarkMode(previous=>!previous)}
-            >{!darkMode?<BsMoon size="1.3em"/>:<MdWbSunny className="sun-icon" size="1.3em"/>}</button>
+            <Tippy content={<small>Dark/Light Mode Toggle</small>}>
+                <button
+                    className="togg-btn"
+                    onClick={()=>handleDarkMode(previous=>!previous)}
+                >{!darkMode?<BsMoon size="1.3em"/>:<MdWbSunny className="sun-icon" size="1.3em"/>}</button>
+            </Tippy>
         </div>
     )
 }
